dd470ee6a8bdad79090dc3f7d69665763b98207d
bibraj96 <blendi.ibraj96@gmail.com>
2021-02-02 16:02:01 -0500

    pass albums down as props to Album component and add semantic tags

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Album from './components/Album'
 import Navbar from './components/Navbar'
+import albums from './api/api'
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Album />
+      <header id="header">
+        <Navbar />
+      </header>
+      <main id="main">
+        <Album albums={albums}/>
+      </main>
     </div>
   );
 }
