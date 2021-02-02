d12ac4288ae3e869fe56bec4384a9bb564b5172d
bibraj96 <blendi.ibraj96@gmail.com>
2021-02-02 16:01:45 -0500

    iterate thru albums prop and display each album

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,8 +1,17 @@
-const Album = () => {
+const Album = ({ albums }) => {
   return (
     <div className="container">
       <div className="albums">
-        <p>Albums</p>
+        <div className="albums-list">
+          {albums.map(album => (
+            <div key={album.id} className="album-item">
+              <h3>{album.title}</h3>
+              <h4>{album.artist}</h4>
+              <p>{album.genre}</p>
+              <p>{album.price}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
