43bd759e836c7b8bbb88a703597cc234becaf724
bibraj96 <blendi.ibraj96@gmail.com>
2021-02-03 21:09:48 -0500

    add margin to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,10 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       <h2>Sidebar</h2>
+      <p>Artists</p>
+      <p>Genres</p>
+      <p>Reviews</p>
+      <p>About us</p>
     </div>
   )
 }
