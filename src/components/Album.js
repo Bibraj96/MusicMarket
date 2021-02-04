d126dd205dddf01edf0ad884b9b9ebdb1e190ead
bibraj96 <blendi.ibraj96@gmail.com>
2021-02-03 20:14:19 -0500

    delete CRA logos and pics

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,14 +1,16 @@
 const Album = ({ albums }) => {
+
   return (
     <div className="container">
       <div className="albums">
         <div className="albums-list">
           {albums.map(album => (
             <div key={album.id} className="album-item">
-              <h3>{album.title}</h3>
-              <h4>{album.artist}</h4>
-              <p>{album.genre}</p>
-              <p>{album.price}</p>
+              <h3 className="album-title">{album.title}</h3>
+              <h4 className="album-artist">{album.artist}</h4>
+              <p className="album-genre">{album.genre}</p>
+              <p className="album-price">{album.price}</p>
+              <button className="album-btn">Add to Cart</button>
             </div>
           ))}
         </div>
