4982e87c79098bd3ee40df73568027d29173bf7f
bibraj96 <blendi.ibraj96@gmail.com>
2021-02-05 14:57:47 -0500

    remove mapping thru albums and move it to map so we can just format in this comp

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,20 +1,12 @@
-const Album = ({ albums }) => {
+
+const Album = ({ title, artist, genre, price }) => {
 
   return (
-    <div className="container">
-      <div className="albums">
-        <div className="albums-list">
-          {albums.map(album => (
-            <div key={album.id} className="album-item">
-              <h3 className="album-title">{album.title}</h3>
-              <h4 className="album-artist">{album.artist}</h4>
-              <p className="album-genre">{album.genre}</p>
-              <p className="album-price">${album.price}</p>
-              <button className="album-btn">Add to Cart</button>
-            </div>
-          ))}
-        </div>
-      </div>
+    <div className="album-item">
+      <h3 className="album-title">{title}</h3>
+      <h4 className="album-artist">{artist}</h4>
+      <p className="album-genre">{genre}</p>
+      <p className="album-price">${price}</p>
     </div>
   )
 }
