10edf11e8d325f58382782a20a1929e3e9fc383f
bibraj96 <blendi.ibraj96@gmail.com>
2021-02-05 14:57:24 -0500

    move mapping thru albums to app when rendering Album

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import './App.css'
+import React, { useState } from 'react'
+// import Cart from './Cart'
 import Album from './components/Album'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import albums from './api/api'
 
-function App() {
+const App = () => {
+
+  const [albumsInCart, setAlbumsInCart] = useState([]);
+
+  const handleAddtoCartClick = id => {
+    setAlbumsInCart(albumsInCart => {
+      const albumInCart = albumsInCart.find(album => album.id === id);
+
+      if (albumInCart) {
+        return albumsInCart.map(album => {
+          if (album.id !== id) return album
+          return { ...albumInCart, quantity: album.quantity + 1}
+        })
+      }
+      const album = albums.find(album => album.id === id)
+      return [...albumsInCart, { ...album, quantity: 1 }]
+    })
+  }
+
+  const totalCost = albumsInCart.reduce(
+    (acc, album) => acc + album.price * album.quantity, 0
+  )
+
   return (
     <div className="App">
       <header id="header">
@@ -12,7 +36,18 @@ function App() {
       </header>
       <main id="main">
         <Sidebar />
-        <Album albums={albums}/>
+        <div className="albums-list">
+          {albums.map(album => (
+            <Album
+              key={album.id}
+              title={album.title}
+              artist={album.artist}
+              genre={album.genre}
+              price={album.price}
+            />
+          ))}
+        </div>
+        {/* <Cart albums={albumsInCart} totalCost={totalCost}/> */}
       </main>
     </div>
   );
