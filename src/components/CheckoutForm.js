2407a0712f89cb7b701abcb9b4482ac02b19cbaa
bibraj96 <blendi.ibraj96@gmail.com>
2021-02-10 14:40:17 -0500

    complete checkout form

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,7 +1,56 @@
-import { CardElement, injectStripe } from 'react-stripe-elements'
+import React, { useState } from 'react';
+import { CardElement, injectStripe } from 'react-stripe-elements';
 
-const CheckoutForm = () => {
 
+const CheckoutForm = ({ stripe, totalCost }) => {
+  const [status, setStatus] = useState('default');
+
+  const submit = async e => {
+    e.preventDefault();
+
+    setStatus('submitting');
+
+    try {
+      let { token } = await stripe.createToken({ name: 'Name' });
+
+      let response = await fetch('/.netlify/functions/charge', {
+        method: 'POST',
+        body: JSON.stringify({
+          amount: totalCost * 100,
+          token: token.id,
+        }),
+      });
+
+      if (response.ok) {
+        setStatus('complete');
+      } else {
+        throw new Error('Network response failed.');
+      }
+    } catch (err) {
+      setStatus('error');
+    }
+  };
+
+  if (status === 'complete') {
+    return <div className="checkoutForm-complete">Payment successful!</div>;
+  }
+
+  return (
+    <form className="checkoutform" onSubmit={submit}>
+      <h4>Would you like to complete the purchase?</h4>
+      <CardElement />
+      <button
+        className="checkoutform-button"
+        type="submit"
+        disabled={status === 'submitting'}
+      >
+        {status === 'submitting' ? 'Submitting' : 'Submit Order'}
+      </button>
+      {status === 'error' && (
+        <div className="checkoutform-error">Something went wrong.</div>
+      )}
+    </form>
+  );
 }
 
-export default CheckoutForm
+export default injectStripe(CheckoutForm)
