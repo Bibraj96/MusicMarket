b6872425d260be480e8ea15cb58bf1901196a15a
bibraj96 <blendi.ibraj96@gmail.com>
2021-02-12 16:14:39 -0500

    change p tags into a tags

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <h1><i className="fas fa-record-vinyl"></i>MusicMarket</h1>
-      <p>Artists</p>
-      <p>Albums</p>
-      <p>Genres</p>
+      <a href="#">Artists</a>
+      <a href="#">Albums</a>
+      <a href="#">Genres</a>
     </nav>
   )
 }
